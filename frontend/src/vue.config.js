63ab7b187f48037e4ad8928795ad2576ff674b30
martyfibghtov <artemnurmatov@gmail.com>
2023-02-18 00:45:45 +0100

    #1 Reinit django and update nginx to work with it correctly.

--- frontend/src/vue.config.js
+++ frontend/src/vue.config.js
@@ -1,8 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-  allowedHosts: [
-    '0.0.0.0'
-  ]
-});
+//module.exports = defineConfig({
+//  transpileDependencies: true,
+//  lintOnSave: false,
+//  allowedHosts: [
+//    '0.0.0.0'
+//  ]
+//});
+module.exports = {
+  devServer: {
+    allowedHosts: "all",
+  }
+}
